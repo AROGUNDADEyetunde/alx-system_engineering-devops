53b214b3d31a9b7af4e633de8041f36aca30b50e
AROGUNDADEyetunde <arogundadeyetunde1994@gmail.com>
2024-03-24 12:43:48 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file { '/etc/ssh/ssh_config':
+    ensure  => present,
+    content => "
+# SSH client configuration
+Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
+}
